7ddfa56716dd2e1aaf0175f5b3a2977fc130f3a7
Tzu-Yin Hong <lili668668@gmail.com>
2019-08-25 15:18:14 +0800

    feat: draw color

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+extern crate termion;
+
 use std::io;
+use termion::{color, style};
 
 struct Subject {
     question: String,
@@ -14,16 +17,16 @@ fn main() {
     ];
     for subject in &subjects {
         loop {
-            println!("問題：{}", subject.question);
+            println!("{}{}問題：{}{}", color::Fg(color::Cyan), style::Bold, subject.question, style::Reset);
             let mut guess = String::new();
-            println!("請輸入答案：");
+            println!("{}請輸入答案：{}", color::Fg(color::LightBlack), style::Reset);
             io::stdin().read_line(&mut guess)
                 .expect("請輸入一些文字");
             if guess.trim() == subject.answer {
-                println!("答對了，你好棒 owo");
+                println!("{}答對了，你好棒 owo{}", color::Fg(color::Yellow), style::Reset);
                 break;
             } else {
-                println!("叭叭，答錯了 OAO");
+                println!("{}叭叭，答錯了 OAO{}", color::Fg(color::LightRed), style::Reset);
             }
         }
     }
