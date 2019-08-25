997a341138d1fcfa531af0edd95463609e3132b0
Tzu-Yin Hong <lili668668@gmail.com>
2019-08-25 14:53:47 +0800

    feat: make question system

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,18 @@ fn main() {
         }
     ];
     for subject in &subjects {
-        println!("{}", subject.question);
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-            .expect("請輸入一些文字");
-        if guess.trim() == subject.answer {
-            println!("答對了，你好棒 owo");
-        } else {
-            println!("叭叭，答錯了 OAO");
+        loop {
+            println!("問題：{}", subject.question);
+            let mut guess = String::new();
+            println!("請輸入答案：");
+            io::stdin().read_line(&mut guess)
+                .expect("請輸入一些文字");
+            if guess.trim() == subject.answer {
+                println!("答對了，你好棒 owo");
+                break;
+            } else {
+                println!("叭叭，答錯了 OAO");
+            }
         }
     }
 }
